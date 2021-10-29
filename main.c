12a4f4b75b2be82e0448826945853c6df615d6fa
Patrick Arntson <arntsonp@os1.engr.oregonstate.edu>
2021-10-28 18:17:18 -0700

    mostly functional

--- main.c
+++ main.c
@@ -36,13 +36,15 @@ struct commandLine *parseCommand(char *command){
 
     char *saveptr;
     int i = 0;
+    // have to initiate background to 0
+    currCommand->background = 0;
 
     // get command (putting command as first ele in args array instead of own struct ele currently)
     char *token = strtok_r(command, " ", &saveptr);
     currCommand->args[i] = calloc(strlen(token) + 1, sizeof(char));
     // currCommand->command = calloc(strlen(token) + 1, sizeof(char));
     // if line starts with #, line is comment
-    if (strncmp(COMMENT, token, 1) == 0){
+    if (strncmp(COMMENT, token, 1) == 0 || token == NULL){
         currCommand->comment = 1;
         return currCommand;
     }
@@ -105,6 +107,105 @@ struct commandLine *parseCommand(char *command){
     return currCommand;
 }
 
+
+// This function executes the user given command and handles input, output, and background info
+int executeCommand(struct commandLine* currCommand){
+    
+    // if inputFile is specified
+    if(currCommand->inputFile != NULL){
+        // Open source file
+        int sourceFD = open(currCommand->inputFile, O_RDONLY);
+        if (sourceFD == -1) { 
+            // maybe dont use printf here? maybe use write function if issue arises
+            printf("cannot open %s for input \n", currCommand->inputFile); 
+            exit(1); 
+        }
+        // // Written to terminal
+        // printf("sourceFD == %d\n", sourceFD); 
+
+        // Redirect stdin to source file
+        int result = dup2(sourceFD, 0);
+        if (result == -1) { 
+            perror("source dup2()"); 
+            exit(2); 
+        }
+    }
+
+    // if outputFile is specified
+    if(currCommand->outputFile != NULL){
+        // Open target file
+        int targetFD = open(currCommand->outputFile, O_WRONLY | O_CREAT | O_TRUNC, 0640);
+        if (targetFD == -1) { 
+            perror("target open()"); 
+            exit(1); 
+        }
+
+        // // Written to terminal
+        // printf("targetFD == %d\n", targetFD); 
+    
+        // Redirect stdout to target file
+        int result = dup2(targetFD, 1);
+        if (result == -1) { 
+            perror("target dup2()"); 
+            exit(2); 
+        }
+    }
+
+    // if background is true and no specified outputfile is given, output is /dev/null (output is muted bc its background process)
+    if(currCommand->background == 1){
+        // if output not specified
+        if(currCommand->outputFile == NULL){
+            // Open target file
+            int targetFD = open("/dev/null", O_WRONLY | O_CREAT | O_TRUNC, 0640);
+            if (targetFD == -1) { 
+                perror("target open()"); 
+                exit(1); 
+            }
+
+            // // Written to terminal
+            // printf("targetFD == %d\n", targetFD); 
+        
+            // Redirect stdout to target file
+            int result = dup2(targetFD, 1);
+            if (result == -1) { 
+                perror("target dup2()"); 
+                exit(2); 
+            }
+        }
+
+        // if input not specified
+        if(currCommand->inputFile == NULL){
+            // Open source file
+            int sourceFD = open("/dev/null", O_RDONLY);
+            if (sourceFD == -1) { 
+                perror("source open()"); 
+                exit(1); 
+            }
+            // // Written to terminal
+            // printf("sourceFD == %d\n", sourceFD); 
+
+            // Redirect stdin to source file
+            int result = dup2(sourceFD, 0);
+            if (result == -1) { 
+                perror("source dup2()"); 
+                exit(2); 
+            }
+        }
+
+    }
+
+    // execute the command
+    execvp(currCommand->args[0], currCommand->args);
+    perror(currCommand->args[0]);
+    exit(2);
+    
+    return 2;
+}
+
+
+
+
+
 // I couldnt get fflush(stdin) to do anything about stopping an infinite loop, so I found this on stack overflow
 // https://stackoverflow.com/questions/53474700/c-scanf-did-not-stop-in-infinite-while-loop
 void empty_stdin(void) {
@@ -115,37 +216,49 @@ void empty_stdin(void) {
 }
 
 
-
 int main(){
 	
     char response[2049];
     int foregroundStatus = 0;
     int *statusptr;
     statusptr = &foregroundStatus;
-    pid_t spawnpid;
+    pid_t spawnPid;
+    int childStatus;
+    pid_t *backgroundPid;
 
 
     while(1){
-        waitpid()
+    
+        pid_t childCheck = waitpid(-1, &childStatus, WNOHANG);
+        if (childCheck != 0 && childCheck != -1){
+            printf("Background pid %d is done \n", childCheck);
+            fflush(stdout);
+        }
         printf(": ");
         fflush(stdout);
         scanf("%[^\n]s", &response);
+        // without this empty_stdin function, the loop would be infinite
         empty_stdin();
 
         // exit command
         if (strcmp(response, "exit")==0){
-            break;
+            killpg(getpid(), SIGTERM);
+            return 0;
         }
 
-        // comment line
+        // comment line command
         if (strlen(response) == 0 || response == NULL){
-            printf("comment worked\n");
-            fflush(stdout);
-            break;
+            continue;
         }
 
         else {
+
             struct commandLine *parsedResponse = parseCommand(response);
+
+            // if multiple spaces (i.e. comment line)
+            if (parsedResponse->comment == 1){
+                continue;
+            }
 
             // cd command
             if (strcmp(parsedResponse->args[0], "cd") == 0){
@@ -164,29 +277,47 @@ int main(){
             if (strcmp(parsedResponse->args[0], "status")==0){
                 printf("%d \n", *statusptr);
                 fflush(stdout);
+                // continue;
             }
 
+            // execute other commands
+            else {
+                pid_t childPid = fork();
+                switch(childPid){
+                    case -1:
+                        perror("fork()\n");
+                        exit(1);
+                        break;
+                    case 0:
+                        executeCommand(parsedResponse);
+                    default:
+                        // if foreground process
+                        if (parsedResponse->background == 0){
+                           waitpid(childPid, &childStatus, 0); 
+                           foregroundStatus = childStatus;
+                        //    continue;
+                        } 
+                        // if background process
+                        else {
+                            printf("background pid is %d\n", childPid);
+                            fflush(stdout);
+                            // continue;
+                        }
+                }
 
-            // printf("%d \n", parsedResponse->comment);
-            // fflush(stdout);
+            }
 
-            // printf("%s \n", parsedResponse->command);
-            // fflush(stdout);
-            // for (int i = 0; parsedResponse->args[i] != '\0'; i++){
-            //     printf("%s \n", parsedResponse->args[i]);
-            //     fflush(stdout);
-            // }
-
-            // printf("%s \n", parsedResponse->inputFile); 
-            // fflush(stdout);
-
-            // printf("%s \n", parsedResponse->outputFile);
-            // fflush(stdout);
-
-            // printf("%d \n", parsedResponse->background);
-            // fflush(stdout);
-            // continue;
-
+        }
+        childCheck = waitpid(0, &childStatus, WNOHANG);
+        if (childCheck != 0 && childCheck != -1){
+            while(1){
+                printf("Background pid %d is done \n", childCheck);
+                fflush(stdout);
+                childCheck = waitpid(0, &childStatus, WNOHANG);
+                if (childCheck == 0 || childCheck == -1){
+                    break;
+                }
+            }
         }
     }
 
